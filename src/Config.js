882c28f6710f19b66ccedbe6f0b39bcb417bf08f
Sridhar-Shankar <ssridhar571999@gmail.com>
2024-11-02 11:14:16 +0530

    ADD FRONTEND

---
+++ src/Config.js
@@ -0,0 +1,18 @@
+// Export a common base URL for making API requests
+export const commonapiurl = "http://localhost:5000/api/";
+
+// Export an array of abbreviated month names
+export const montharr = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
