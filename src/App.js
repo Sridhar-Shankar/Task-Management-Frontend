62b89ad0d648f24e9e5f7b5d8a3574e805462dcd
Sridhar-Shankar <ssridhar571999@gmail.com>
2024-11-03 14:51:57 +0530

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 // Import necessary components and hooks
-import Home from "./components/Home/Home";
-import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Readonlychecklist from "./components/Read/Read";
 import { useSelector } from "react-redux";
-import Login from "./components/Login/Login";
+import Login from "./components/Login/Login.jsx";
 
 function App() {
   // Retrieve the 'userid' from the Redux store
-  const userid = useSelector(store => store.user.userid);
+  const userid = useSelector((store) => store.user.userid);
 
   return (
     // Set up the router
     <Router>
       <Routes>
         {/* Define the route for the login/signup page */}
-        <Route exact path="/" element={<Login/>}/>
-        
+        <Route exact path="/" element={<Login />} />
+
         {/* Define protected routes that require the user to be logged in (i.e., have a userid).
             If the user is not logged in, redirect to the login/signup page. */}
-        <Route path="/board" element={userid ? <Home/> : <Navigate replace to="/"/>}/>
-        <Route path="/analytics" element={userid ? <Home/> : <Navigate replace to="/"/>}/>
-        <Route path="/settings" element={userid ? <Home/> : <Navigate replace to="/"/>}/>
-        
+        <Route
+          path="/board"
+          element={userid ? <Home /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/analytics"
+          element={userid ? <Home /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/settings"
+          element={userid ? <Home /> : <Navigate replace to="/" />}
+        />
+
         {/* Define the route for the readonly checklist page, accessible regardless of login status */}
-        <Route path="/readonlychecklist/:id" element={<Readonlychecklist/>}/>
+        <Route path="/readonlychecklist/:id" element={<Readonlychecklist />} />
       </Routes>
     </Router>
   );
