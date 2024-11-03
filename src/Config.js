3e78c5b7d014b443f9492cc177284be539174b5e
Sridhar-Shankar <ssridhar571999@gmail.com>
2024-11-03 14:46:10 +0530

    fixed api

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,6 @@
-// Export a common base URL for making API requests
 export const commonapiurl =
   "https://task-management-backend-mk01.onrender.com/api/";
 
-// Export an array of abbreviated month names
 export const montharr = [
   "Jan",
   "Feb",
